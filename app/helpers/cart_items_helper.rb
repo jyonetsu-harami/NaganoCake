29995796e64ad6fb5337180194510ee10f633a2f
d-hamasato <73775819+d-hamasato@users.noreply.github.com>
2021-01-20 17:41:51 +0000

    [add] EC側 商品詳細, カート機能

--- app/helpers/cart_items_helper.rb
+++ app/helpers/cart_items_helper.rb
@@ -1,2 +1,10 @@
 module CartItemsHelper
+  
+  def total_amount(cart_items)
+    array = []
+    cart_items.each do |cart_item|
+      array << cart_item.product.price*cart_item.amount
+    end
+    array.sum
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,23 +4,23 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
   root 'homes#top'
   get 'about' => 'homes#about'
-  
+
   namespace :admin do
     resources :products, only: [:index, :new, :create, :show, :edit, :update]
-    resources :genres, only: [:index, :create, :update]
+    resources :genres, only: [:index, :create, :update, :edit]
     resources :customers, only: [:index, :show, :edit, :update]
     resources :orders, only: [:index, :show, :update]
     resources :order_items, only: [:update]
   end
-    
-  
+
+
   resources :products, only: [:index, :show]
-  resources :cart_items, only: [:create, :index, :update, :destory] do
-    member do
-      delete :destory_all
+  resources :cart_items, only: [:create, :index, :update, :destroy] do
+    collection do
+      delete :destroy_all
     end
   end
   resources :orders, only: [:new, :create, :index, :show] do
@@ -36,5 +36,5 @@ Rails.application.routes.draw do
     end
   end
   resources :shipping_informations, only: [:index, :create, :edit, :update, :destory]
-  
+
 end
