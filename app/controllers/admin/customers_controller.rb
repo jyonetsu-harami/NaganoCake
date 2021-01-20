e1f29927d7894a19c922fffcc5e5aaceaacaa330
yokotamaki <ymaki0626@gmail.com>
2021-01-20 08:52:06 +0000

    管理者側　ユーザー編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -9,8 +9,31 @@ class Admin::CustomersController < ApplicationController
   end  
   
   def edit
+    @customer = Customer.find(params[:id])
   end
-  def update
-  end  
   
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      redirect_to admin_customers_path
+    end  
+  end
+  
+  private
+  def customer_params
+    params.require(:customer).permit(
+                                    :last_name, 
+                                    :first_name, 
+                                    :last_name_kana, 
+                                    :first_name_kana,
+                                    :zipcode,
+                                    :address,
+                                    :phone_number,
+                                    :email,
+                                    :is_active
+                                    )
+  end
+
 end
