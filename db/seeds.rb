3ebd54b32d196b9e281cc59cdb701668d1bd5cce
EC2 Default User <ec2-user@ip-172-31-27-154.ec2.internal>
2021-01-20 04:12:24 +0000

    [カスタマー側のマイページ]

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,10 @@ Admin.find_or_create_by(id: 1) do |admin|
  admin.password = "aaaaaa"
 end
 
+Genre.create!(
+  name: 'ケーキ',
+  is_active: 'true',
+)
 Product.create!(
   genre_id: 1,
   image_id: '1',
@@ -18,10 +22,6 @@ Product.create!(
   description: '美味しいです。',
   price: 600,
   sales_status: 0,
-)
-Genre.create!(
-  name: 'ケーキ',
-  is_active: 'true',
 )
 
 Customer.create!(
