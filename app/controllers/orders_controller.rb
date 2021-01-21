3ac0e68d986c803b5c4db52816283a98ca5da5b3
yokotamaki <ymaki0626@gmail.com>
2021-01-21 04:10:31 +0000

    注文入力画面（途中）　mergeするためにコミット

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,16 @@
 class OrdersController < ApplicationController
+  
+  def new
+    @order = Order.new
+    # customer = @order.customer
+    # @info = customer.shipping_informations
+  end  
+  
+  def create
+  end
+  
+  private
+  def order_params
+    params.require(:order).permit(:zipcode, :address, :name, :payment_method)
+  end
 end
