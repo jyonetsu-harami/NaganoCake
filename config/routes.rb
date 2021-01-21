7162f42947fdd0797eb905b201c4968149b95295
d-hamasato <73775819+d-hamasato@users.noreply.github.com>
2021-01-21 15:59:35 +0000

    [add]新規会員登録機能, sign_in, sign_upビュー

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'homes/top'
   get 'homes/about'
   devise_for :admins
-  devise_for :customers
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :customers, controllers: {
+    sessions: 'customers/sessions',
+    registrations: 'customers/registrations'
+  }
 
   root 'homes#top'
   get 'about' => 'homes#about'
