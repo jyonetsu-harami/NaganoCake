8373ee2f13ad2b3bbf8443d7723a014870106fe2
d-hamasato <73775819+d-hamasato@users.noreply.github.com>
2021-01-21 16:28:31 +0000

    [update]非アクティブ会員はログインできないように

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,8 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :shipping_informations, dependent: :destroy
+  
+  def active_for_authentication?
+    super && self.is_active == true
+  end
 end
