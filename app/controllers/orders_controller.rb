7fc0a831c39b9024694b3881667e214b0b5f72d7
yokotamaki <ymaki0626@gmail.com>
2021-01-22 11:35:45 +0000

    注文情報入力画面（ラジオボタンの記述変更 他 試行錯誤中）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,28 +2,35 @@ class OrdersController < ApplicationController
   
   def new
     @order = Order.new
+    # @shipping_info = current_customer.shipping_informations
   end  
   
   def create
-    address_attribute = params[:sipping_address]      
-    @order = Order.new
-    if address_attribute == 1
+    @order = Order.new(order_params)
+    @shipping_attribute = params["shipping_attribute"]
+    #ご自身の住所 
+    if @shipping_attribute == "1"
       @order.zipcode = current_customer.zipcode
       @order.address = current_customer.address
-      @order.name = current_customer.last_name +current_customer.first_name
-    elsif address_attribute == 2
-      
-    elsif address_attribute == 3
-      
-    end
-      
+      @order.name = current_customer.last_name + current_customer.first_name
+    #登録済の住所
+    elsif @shipping_attribute == "2"
+      # @shipping_info = params[:customer_id]
+      # @shipping_info = ShippingInformation.new(shipping_info_params)
+      # @shipping_info.zipcode = 
+      # @shipping_info.address =
+      # @shipping_info.name = 
+    #新しいお届け先
+    elsif @shippings_attribute == "3"
+      # @new_address = Order.new(order_params)
+      # @new_address.zipcode = params[:zipcode]
+      # @new_address.address = params[:address]
+      # @new_address.name = params[:name]
     end
     render :order_confirm
   end
   
   def order_confirm
-    @order = Order.new(order_params)
-    @shipping_address = params([:shipping_address])
   end
   
   def index
@@ -32,6 +39,10 @@ class OrdersController < ApplicationController
   
   private
   def order_params
-    params.require(:order).permit(:zipcode, :address, :name, :payment_method, :shipping_address)
+    params.require(:order).permit(:zipcode, :address, :name, :payment_method, :sipping_attribute)
   end
+  
+  # def shipping_info_params
+  #   params.require(:shipping_information).permit(:zipcode, :address, :name)
+  # end  
 end
--- app/models/shipping_information.rb
+++ app/models/shipping_information.rb
@@ -1,3 +1,9 @@
 class ShippingInformation < ApplicationRecord
-    belongs_to :customer
+  belongs_to :customer
+    
+  #配送先住所の結合(注文関連で使用)
+  def address_all
+    "〒" + self.zipcode + "　" + self.address + "　" + self.name 
+  end  
+
 end
