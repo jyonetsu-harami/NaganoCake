753558c1645df4591f46200281546c61565fbb77
d-hamasato <73775819+d-hamasato@users.noreply.github.com>
2021-01-24 13:53:00 +0000

    [add]admin側 注文一覧、詳細、ステータス変更機能

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,26 @@
 class Admin::OrdersController < ApplicationController
+  
+  def index
+    @orders = Order.page(params[:page]).reverse_order
+  end
+  
+  def show
+    @order = Order.find(params[:id])
+    @customer = Customer.find(@order.customer_id)
+    @order_items = @order.order_items
+  end
+  
+  def update
+    order = Order.find(params[:id])
+    if order.update(order_params) && params[:order][:status] = "入金確認"
+      order.order_items.update_(making_status: "制作待ち")
+    end
+    redirect_back fallback_location: root_path
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,2 +1,9 @@
 module Admin::OrdersHelper
+  def order_quantity(order)
+    array = []
+    order.order_items.each do |order_item|
+      array << order_item.amount
+    end
+    array.sum
+  end
 end
