8da4fb14eaded91328a4edeea8c7442dc533cde2
d-hamasato <73775819+d-hamasato@users.noreply.github.com>
2021-01-24 15:11:23 +0000

    [update]商品個数の合計

--- app/helpers/admin/orders_helper.rb
+++ app/helpers/admin/orders_helper.rb
@@ -1,9 +1,2 @@
 module Admin::OrdersHelper
-  def order_quantity(order)
-    array = []
-    order.order_items.each do |order_item|
-      array << order_item.amount
-    end
-    array.sum
-  end
 end
